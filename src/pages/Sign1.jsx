ede52b73edc2843080634d6c10c3885deffe49bc
amrutha-ranjakumar <147486960+amrutha-ranjakumar@users.noreply.github.com>
2024-06-04 20:32:12 +0530

    music player using react

--- src/pages/Sign1.jsx
+++ src/pages/Sign1.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import music1 from '../assets/front3.jpg';
 import Form from 'react-bootstrap/Form';
 import { loginAPI, registerAPI } from '../services/allAPI';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
-import Swal from 'sweetalert2';
+// import img from '../assets/front6.jpg';
+import Swal from 'sweetalert2'
 
 function Sign1({ register }) {
   const registerForm = register ? true : false;
@@ -18,19 +20,21 @@ function Sign1({ register }) {
 
   const handleRegister = async (e) => {
     e.preventDefault();
+    console.log(userData);
     const { username, email, password } = userData;
     if (!username || !email || !password) {
       Swal.fire({
         title: "REGISTRATION?",
-        text: "Please fill the form completely?",
+        text: "please fill the form completely?",
         icon: "question"
       });
     } else {
       const result = await registerAPI(userData);
+      console.log(result);
       if (result.status === 200) {
         Swal.fire({
           title: "REGISTRATION?",
-          text: "User registered successfully",
+          text: " User Registration successfully ?",
           icon: "success"
         });
         setUserData({
@@ -51,7 +55,7 @@ function Sign1({ register }) {
     if (!email || !password) {
       Swal.fire({
         title: "LOGIN",
-        text: "Please fill the form completely?",
+        text: "please fill the form completely?",
         icon: "question"
       });
     } else {
@@ -61,7 +65,7 @@ function Sign1({ register }) {
         sessionStorage.setItem('token', result.data.token);
         Swal.fire({
           title: "LOGIN?",
-          text: "User logged in successfully",
+          text: "user logged in successfully",
           icon: "success"
         });
         setUserData({
@@ -78,13 +82,14 @@ function Sign1({ register }) {
 
   return (
     <>
-      <div style={{ backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', color: 'white' }}>
+      <div style={{  backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh', color: 'white' }}>
+        <div className='mb-5 mt-2 p-1'></div>
         <div className='d-flex justify-content-center align-items-center' style={{ minHeight: '90vh' }}>
-          <div className='container'>
-            <div className='card card-5 border p-5 mt-3'>
+          <div className='w-100 container'>
+            <div className='card card-5 border boder-5 p-5 mt-3'>
               <div className='row align-items-center'>
                 <div className='col-lg-6 col-md-6 p-5'>
-                  <img src='https://i.pinimg.com/564x/a1/a9/3e/a1a93ed20a5f0ab9fbb36d8269572c40.jpg' alt='' className='img-fluid' style={{ borderRadius: '10%' }} />
+                  <img src='https://i.pinimg.com/564x/a1/a9/3e/a1a93ed20a5f0ab9fbb36d8269572c40.jpg'alt='' width={'100%'} height={'700px'} style={{ borderRadius: '10%' }} />
                 </div>
                 <div className='col-lg-6 col-md-6 p-3'>
                   <div className='d-flex align-items-center flex-column'>
@@ -93,11 +98,13 @@ function Sign1({ register }) {
                     </h2>
                     <h5 style={{ color: 'blue' }}>{registerForm ? 'Sign Up Your Account' : 'Sign Into Your Account'}</h5>
                     <br />
-                    <Form className='w-100'>
+                    <Form className='w-100'  >
                       {registerForm && (
-                        <Form.Group controlId='formUsername' className='mb-3'>
+                    
+                        <Form.Group controlId='formUsername' className='mb-3 '>
                           <Form.Label className='text-primary'>User Name</Form.Label>
                           <Form.Control
+                          
                             value={userData.username}
                             onChange={(e) => setUserData({ ...userData, username: e.target.value })}
                             size='lg'
@@ -107,7 +114,7 @@ function Sign1({ register }) {
                         </Form.Group>
                       )}
                       <Form.Group controlId='formEmail' className='mb-3'>
-                        <Form.Label className='text-primary'>Email</Form.Label>
+                        <Form.Label  className='text-primary'>Email</Form.Label>
                         <Form.Control
                           value={userData.email}
                           onChange={(e) => setUserData({ ...userData, email: e.target.value })}
@@ -134,7 +141,7 @@ function Sign1({ register }) {
                         </button>
                         <p className='mt-3'>
                           Already a user? Click here to{' '}
-                          <Link to='/sign' style={{ textDecoration: 'none', color: 'blue' }}>
+                          <Link to='/sign' style={{ textDecoration: 'none', color: 'white' }}>
                             Login
                           </Link>
                         </p>
@@ -146,7 +153,7 @@ function Sign1({ register }) {
                         </button>
                         <p className='mt-3'>
                           Not a user yet? Click here to{' '}
-                          <Link to='/register' style={{ textDecoration: 'none', color: 'blue' }}>
+                          <Link to='/register' style={{ textDecoration: 'none', color: 'white' }}>
                             Register
                           </Link>
                         </p>
@@ -159,7 +166,11 @@ function Sign1({ register }) {
           </div>
         </div>
       </div>
-      <ToastContainer position='top-center' autoClose={2000} theme='dark' />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        theme='dark'
+      />
     </>
   );
 }
